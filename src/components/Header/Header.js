085ecad6c53ecad5562b63532feb97701641be54
Lan-ce-lot <1984737645@qq.com>
2021-12-28 17:18:01 +0800

    :tada: Initial commit

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,46 +8,48 @@ import "./Header.css"
 import React from "react"
 import {FormControl, InputLabel, MenuItem, Select, TextField} from "@mui/material";
 import countries from "../../data/category";
-import { debounce } from "lodash";
+import {debounce} from "lodash";
+
 const Header = ({
                   category, setCategory, word, setWord, setMeanings, LightTheme,
                 }) => {
   const handleChange = (e) => {
     setCategory(e.target.value);
-    setWord("");
+    setWord(word);
     setMeanings([]);
   };
-    const handleText = debounce((text) => {
+  const handleText = debounce((text) => {
     setWord(text);
   }, 500);
   return (<div className="header">
-      <span className="title">{word ? word : "Word Hunt"}</span>
-      <div className="inputs">
-        <TextField
-          className="search"
-          sx={{minWidth: "200px"}}
-          label="Word"
-          // value={word}
-          onChange={(e) => handleText(e.target.value)}
-        />
-        <FormControl
-          className="select"
-          sx={{ minWidth: "200px"}}
+    <span className="title">{word ? word : "Word Hunt"}</span>
+    <div className="inputs">
+      <TextField
+        className="search"
+        sx={{minWidth: "200px"}}
+        label="Word"
+        // value={word}
+        onChange={(e) => handleText(e.target.value)}
+      />
+      <FormControl
+        className="select"
+        sx={{minWidth: "200px"}}
+      >
+        <InputLabel>Language
+        </InputLabel>
+        <Select
+          label="Language"
+          value={category}
+          onChange={(e) => handleChange(e)}
         >
-          <InputLabel>Language
-          </InputLabel>
-          <Select
-            label="Language"
-            value={category}
-            onChange={(e) => handleChange(e)}
-          >
-            {countries.map((option) => (<MenuItem key={option.label} value={option.label}>
-                {option.value}
-              </MenuItem>))}</Select>
-        </FormControl>
-      </div>
+          {countries.map((option) => (<MenuItem key={option.label} value={option.label}>
+            {option.value}
+          </MenuItem>))}
+        </Select>
+      </FormControl>
+    </div>
 
-    </div>)
+  </div>)
 }
 
 export default Header
